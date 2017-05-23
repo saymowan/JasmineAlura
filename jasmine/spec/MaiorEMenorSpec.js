eaa988522d982947a97da821e2ba6937eb68df97
saymowan <saymowan@gmail.com>
2017-05-22 23:16:10 -0300

    Aula 1 Finalizada

--- jasmine/spec/MaiorEMenorSpec.js
+++ jasmine/spec/MaiorEMenorSpec.js
@@ -8,7 +8,38 @@ describe("Maior E Menor", function(){
         expect(algoritmo.pegaMaior()).toEqual(15);
         expect(algoritmo.pegaMenor()).toEqual(5);
 
-    }); 
+    	}); 
+
+    	it("deve entender numeros em ordem decrescente", function() {
+        var algoritmo = new MaiorEMenor();
+        algoritmo.encontra([10,9,8,7]);
+
+        expect(algoritmo.pegaMaior()).toEqual(10);
+        expect(algoritmo.pegaMenor()).toEqual(7);
+
+    	}); 
+
+
+    	it("deve entender numeros em ordem crescente", function() {
+        var algoritmo = new MaiorEMenor();
+        algoritmo.encontra([2,3,6,88,100]);
+
+        expect(algoritmo.pegaMaior()).toEqual(100);
+        expect(algoritmo.pegaMenor()).toEqual(2);
+
+    	}); 
+
+
+    	it("deve entender somente uma entrada", function() {
+        var algoritmo = new MaiorEMenor();
+        algoritmo.encontra([1]);
+
+        expect(algoritmo.pegaMaior()).toEqual(1);
+        expect(algoritmo.pegaMenor()).toEqual(1);
+
+    	}); 
+
+
 
 
 });
