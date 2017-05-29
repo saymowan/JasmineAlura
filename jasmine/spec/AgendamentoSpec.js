b9076949ce0f94b5f4bd8d9d8ba421977326ac48
saymowan <saymowan@gmail.com>
2017-05-28 23:40:43 -0300

    Aula5.final

--- jasmine/spec/AgendamentoSpec.js
+++ jasmine/spec/AgendamentoSpec.js
@@ -1,20 +1,29 @@
 describe("Agendamento", function(){
+	var saymon;
+	var agenda;
 
+
+	beforeEach(function(){
+		saymon = new PacienteBuilder().constroi();
+		agenda 	  = new Agendamento();
+	});
 
 
 	it("deve agendar para 20 dias depois", function(){
-		var guilherme = new PacienteBuilder().constroi();
-		var agenda 	  = new Agendamento();
-
-
-		var consulta  = new Consulta(guilherme,[], false, false, new Date(2014, 7,1));
+		var consulta  = new Consulta(saymon,[], false, false, new Date(2014, 7, 1));
 		var novaconsulta = new agenda.para(consulta);
-
-
 		expect(novaconsulta.getData().toString()).toEqual(new Date(2014, 7, 21).toString());
 	});
 
 
+	it("deve pular finais de semana", function(){ 
+		var consulta  = new Consulta(saymon,[], false, false, new Date(2014, 5, 30));
+		var novaConsulta = agenda.para(consulta);
+
+		expect(novaConsulta.getData().toString()).toEqual(new Date(2014, 6, 21).toString());
+
+
+	});
 
 
 });
--- jasmine/src/Agendamento.js
+++ jasmine/src/Agendamento.js
@@ -5,7 +5,12 @@ function Agendamento(){
 
 		para : function(consulta){
 			var vinteDiasEmMilisegundos = 1000 * 60 * 60 * 24 * 20;
+			var umDiaEmMilisegundo = 1000 * 60 * 60 *24;
+
+			
 			var novaData = new Date(consulta.getData().getTime() + vinteDiasEmMilisegundos);
+			while (novaData.getDay() == 0 || novaData.getDay == 6) 
+				novaData = new Date(novaData.getTime() + umDiaEmMilisegundo);
 
 			var novaConsulta = new Consulta(consulta.getNome(), consulta.getProcedimentos(),consulta.isParticular(),true, novaData);
 			return novaConsulta; 
